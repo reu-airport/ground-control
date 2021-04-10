51a09818bddc6a19351ad2bfeb26445c61d2308c
Daniil Viliavin <89505105005@mail.ru>
2021-04-10 15:50:40 +0300

    Change rabbitmq configuration

--- src/main/kotlin/ru/reu/time/configuration/RabbitmqConfiguration.kt
+++ src/main/kotlin/ru/reu/time/configuration/RabbitmqConfiguration.kt
@@ -10,10 +10,19 @@ class RabbitmqConfiguration {
     @Bean
     fun bindings(): Declarables {
         val queue = Queue("ground-control", true)
+        val queue2 = Queue("movementRequest", true)
+        val queue3 = Queue("movementEnd", true)
+        val queue4 = Queue("movementPermission", true)
         val topicExchange = TopicExchange("")
         return Declarables(
             queue,
-            BindingBuilder.bind(queue).to(topicExchange).with("ground-control")
+            queue2,
+            queue3,
+            queue4,
+            BindingBuilder.bind(queue).to(topicExchange).with("ground-control"),
+            BindingBuilder.bind(queue2).to(topicExchange).with("movementRequest"),
+            BindingBuilder.bind(queue3).to(topicExchange).with("movementEnd"),
+            BindingBuilder.bind(queue4).to(topicExchange).with("movementPermission")
         )
     }
 
